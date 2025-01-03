b6d1cebc0d77f0a159eef6c5d81ee3c21036f0a2
DevAman11 <aman11newus@gmail.com>
2025-01-03 12:09:04 +0530

    Data

--- FrontEnd/Crudapp/src/assets/Components/Home.jsx
+++ FrontEnd/Crudapp/src/assets/Components/Home.jsx
@@ -3,16 +3,28 @@ import React from 'react'
 function Home() {
   return (
      <>
-
+  <body>
      <section>
       <div className='HomeMain'>
-        <div className='HomeParent'>
-           <h1> Aman's Crud App</h1>
+       
+        <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg" class="rotate">
+    <circle cx="200" cy="200" r="100" fill="none" stroke="red" />
+    <text font-size="20" fill="blue">
+      <textPath href="#circlePath" startOffset="0%">
+      Aman's Crud App
+      </textPath>
+    </text>
+    <defs>
+      <path id="circlePath" d="M 200,200 m -100,0 a 100,100 0 1,1 200,0 a 100,100 0 1,1 -200,0" />
+    </defs>
+  </svg>
         </div>
-      </div>
+
       </section>
+      </body>
      </>
   )
 }
+
 
 export default Home
--- FrontEnd/Crudapp/src/assets/Components/Signup.jsx
+++ FrontEnd/Crudapp/src/assets/Components/Signup.jsx
@@ -11,11 +11,11 @@ function Signup() {
           <video className="NewVideo" autoPlay loop muted>
             <source src={video1}/>
           </video>
-          <div className="MainPage flex min-h-fit flex-col justify-center px-4.5 py-6 lg:px-6">
+          <div className="MainPage mt-16 flex min-h-fit flex-col justify-center px-4.5 py-6 lg:px-6">
 
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             {/* <img className="mx-auto h-10 w-auto" src="" alt="" /> */}
-            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-zinc-50">Sign Up to your account</h2>
+            <h2 className="mt-11 text-center text-2xl/9 font-bold tracking-tight text-zinc-50">Sign Up to your account</h2>
           </div>
 
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -50,13 +50,13 @@ function Signup() {
 
               <div>
                 <label for="Phone" className="block text-sm/6 font-medium text-zinc-50">Phone Number</label>
-                <div className="mt-2">
+                <div className="mt-6">
                   <input type="number" name="Phone" id="Phone" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
                 </div>
               </div>
 
-              <div>
-                <button type="submit" className="bg-opacity-10 bg-gradient-to-r from-purple-400 md:from-yellow-500 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign Up</button>
+              <div className="mt-6">
+                <button type="submit" className=" bg-opacity-10 bg-gradient-to-r from-purple-400 md:from-yellow-500 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign Up</button>
               </div>
             </form>
 
