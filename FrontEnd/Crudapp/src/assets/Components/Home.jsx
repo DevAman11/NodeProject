7be534cbe906d7b8d3e1aa075f6ad462823c5821
DevAman11 <aman11newus@gmail.com>
2025-01-20 16:38:02 +0530

    done

--- FrontEnd/Crudapp/src/assets/Components/Home.jsx
+++ FrontEnd/Crudapp/src/assets/Components/Home.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-
+import BgImg from '../Components/Content/pexels-ivan-samkov-8952192.jpg'
 function Home() {
   return (
     <>
-      <section>
-        <div className="HomeMain">
-          <h1>Aman's Crud App </h1>
-        </div>
-      </section>
+
+
+      <div className="relative flex overflow-x-hidden ">
+  <div className="py-12 animate-marquee whitespace-nowrap" >
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+  </div>
+  <img src={BgImg} alt="" className="BgImg"/>
+
+  <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+    <span className="text-4xl mx-4">Aman's Crud App </span>
+  </div>
+
+</div>
+
     </>
   );
 }
