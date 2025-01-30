105c5c7e353df6d15b37665bd8d35ee8851766f4
DevAman11 <aman11newus@gmail.com>
2025-01-30 10:56:08 +0530

    done

--- FrontEnd/Crudapp/src/assets/Components/Home.jsx
+++ FrontEnd/Crudapp/src/assets/Components/Home.jsx
@@ -14,7 +14,6 @@ function Home() {
     <span className="text-4xl mx-4">Aman's Crud App </span>
   </div>
 
-
   <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
     <span className="text-4xl mx-4">Aman's Crud App </span>
     <span className="text-4xl mx-4">Aman's Crud App </span>
